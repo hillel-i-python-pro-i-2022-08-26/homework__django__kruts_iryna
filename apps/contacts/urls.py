7bae47f8b7f7e0bbd057dc95a2032b93f51938c9
Iryna Kruts <i_kruts@ukr.net>
2022-10-15 18:37:15 +0300

    added button "edit" to contacts

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -6,4 +6,5 @@ app_name = "contacts"
 
 urlpatterns = [
     path("", views.show_all_contacts, name="index"),
+    path("<int:pk>", views.edit_contact, name="edit"),
 ]
--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .models import Contacts
+from .forms import ContactsForm
 
 
 def show_all_contacts(request: HttpRequest) -> HttpResponse:
@@ -8,3 +9,27 @@ def show_all_contacts(request: HttpRequest) -> HttpResponse:
     return render(
         request, "contacts/index.html", {"title": "Contacts", "contacts": contacts}
     )
+
+def generate_contacts(request: HttpRequest) -> HttpResponse:
+    pass
+
+def get_contact(request: HttpRequest, pk: int) -> HttpResponse:
+    contact = Contacts.objects.get(pk=pk)
+    return None
+    #return render(
+       # request, 'contacts/get_contact.html', {"title": "Get contact", "contact": contact}
+    #)
+
+def edit_contact(request: HttpRequest, pk: int) -> HttpResponse:
+    contact = Contacts.objects.get(pk=pk)
+    form = ContactsForm(instance=contact)
+    return render(
+        request, 'contacts/edit_contact.html',
+        {
+            "title": "Edit contact",
+            "form": form
+        }
+    )
+
+def delete_contact(request: HttpRequest) -> HttpResponse:
+    pass
