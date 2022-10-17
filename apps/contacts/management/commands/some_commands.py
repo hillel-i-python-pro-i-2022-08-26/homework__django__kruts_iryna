0f62ec8164e6e6133c635e8ec37557cfba09ea94
Iryna Kruts <i_kruts@ukr.net>
2022-10-17 20:59:21 +0300

    added commands

--- apps/contacts/management/commands/some_commands.py
+++ apps/contacts/management/commands/some_commands.py
@@ -8,31 +8,13 @@ from apps.contacts import models
 
 
 class Command(BaseCommand):
-    help = "Generate required amount of contacts"
+    help = "Experimental"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.logger = logging.getLogger("django")
 
-    def add_arguments(self, parser: CommandParser):
-        parser.add_argument(
-            "--amount", type=int, default=10, help="Amount of generated contacts"
-        )
 
     def handle(self, *args, **options):
-        amount: int = options["amount"]
-        self.logger.info(f"Generate {amount} contacts")
+        self.logger.info(f"Experemental")
 
-        current_amount = models.Contacts.objects.all().count()
-        self.logger.info(f"Current amount of contacts: {current_amount}")
-
-        for count, contact in enumerate(generate_contact(amount=amount), start=1):
-            self.logger.info(f"Generate: {amount} of {amount}.Start")
-            contact.is_auto_generated = True
-            contact.save()
-            self.logger.info(f"Generate: {amount} of {amount}. End")
-
-        amount_after_generation = models.Contacts.objects.all().count()
-        self.logger.info(
-            f"Amount of contacts after generation: {amount_after_generation}"
-        )
