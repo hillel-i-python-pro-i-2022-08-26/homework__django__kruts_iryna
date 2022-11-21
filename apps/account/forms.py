8966dfa176b5cea428b1fc4fe1e8bf87eed82271
ira <i_kruts@ukr.net>
2022-11-21 14:31:28 +0200

    added btn login, signup

--- apps/account/forms.py
+++ apps/account/forms.py
@@ -10,7 +10,12 @@ from apps.users.models import User
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(label="Username", widget=forms.TextInput)
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput,
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        error_messages={'unique': "A user with that username already exists."},
+    )
     first_name = forms.CharField(label="Firs name", widget=forms.TextInput, required=False)
     last_name = forms.CharField(label="Last name", widget=forms.TextInput, required=False)
     email = forms.EmailField(label="Email", widget=forms.EmailInput())
@@ -20,3 +25,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "first_name", "last_name", "email", "avatar")
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username", widget=forms.TextInput(attrs={"class": "form-control"}))
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={"class": "form-control"}))
--- core/settings.py
+++ core/settings.py
@@ -170,3 +170,5 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR.joinpath("media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#LOGOUT_REDIRECT_URL = "/"
