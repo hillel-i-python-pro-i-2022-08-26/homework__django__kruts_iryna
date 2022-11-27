2f3051805c9bfec495562465002e19d96878b8d3
ira <i_kruts@ukr.net>
2022-11-27 19:56:16 +0200

    added debug tool bar

--- core/settings.py
+++ core/settings.py
@@ -15,14 +15,12 @@ from pathlib import Path
 # noinspection PyPackageRequirements
 import environ
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 APPS_DIR = BASE_DIR.joinpath("apps")
 
 env = environ.Env()
 env.read_env(BASE_DIR.joinpath(".env"))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -40,9 +38,13 @@ if DEBUG:
             "0.0.0.0",
             "127.0.0.1",
             "localhost",
+            # "http://127.0.0.1:8000/"
         ]
     )
-
+# if DEBUG:
+#     import socket  # only if you haven't already imported this
+#     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+#     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 # Application definition
 
 DJANGO_APPS = [
@@ -52,6 +54,7 @@ DJANGO_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
 ]
 LOCAL_APPS = [
     "apps.base",
@@ -77,6 +80,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -103,7 +108,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -118,7 +122,6 @@ DATABASES = {
         )
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -138,7 +141,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -149,7 +151,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -171,3 +172,7 @@ MEDIA_ROOT = BASE_DIR.joinpath("media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- core/urls.py
+++ core/urls.py
@@ -19,12 +19,20 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     path("admin/", admin.site.urls),
     path("", include("apps.base.urls")),
     path("contacts/", include("apps.contacts.urls")),
     path("session/", include("apps.session.urls")),
 ]
 
-
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
