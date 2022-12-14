3da198983ee73591884a3e45d72398f20a451dc1
ira <i_kruts@ukr.net>
2022-12-14 18:51:51 +0200

    added templates

--- apps/session/views.py
+++ apps/session/views.py
@@ -47,7 +47,8 @@ def session_example_view(request: WSGIRequest | HttpRequest) -> HttpResponse:
 #
 # requests.get('http://127.0.0.1:8000')
 
-class AllSessions(ListView):
+
+class AllSessionsView(ListView):
     model = UserDataRequest
     template_name = "session/middleware_info.html"
 
@@ -55,5 +56,30 @@ class AllSessions(ListView):
         context = super().get_context_data(**kwargs)
         context["title"] = "All session info"
         visitor = UserDataRequest.objects.all()
+        context['object_list'] = visitor
+        return context
+class CurrentSessionViews(ListView):
+    model = UserDataRequest
+    template_name = "session/current_session.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Current session"
+
+        visitor = UserDataRequest.objects.filter(session_key=self.kwargs['session_key']) # some mistake
+
+        context['object_list'] = visitor
+        return context
+
+class CurrentUserViews(ListView):
+    model = UserDataRequest
+    template_name = "session/current_user.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Current user session info"
+
+        visitor = UserDataRequest.objects.filter(user=self.kwargs['pk'])
+
         context['object_list'] = visitor
         return context
